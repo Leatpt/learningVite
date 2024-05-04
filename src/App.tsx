6ade6fadd5c738a8a2bc2a033ef1bc8fcb9449c7
Leatpt <lea.kibur@tptlive.ee>
2024-05-04 15:20:48 +0300

    Using Interface and Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import ListGroup from "./components/ListGroup";
 
-
 function App() {
-  return <div><ListGroup /></div>
+  let items = ["New York", "California", "Tallinn", "London", "Paris"];
+
+  return (
+    <div>
+      <ListGroup items={items} heading="Cities" />
+    </div>
+  );
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,16 @@
 import { useState } from "react";
 
-function ListGroup() {
-  let items = ["New York", "California", "Tallinn", "London", "Paris"];
-  // Hook is a function that allows us to tap into built-in features in react
-  // This is a State Hook, we tell react that we have a function which state
-  // changes over time.
+interface ListGroupProps {
+  items: string[];
+  heading: string;
+}
+
+function ListGroup({ items, heading }: ListGroupProps) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
-  // variable   arr[0]
-  // updater function, this function will update our variable  arr[1]
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
